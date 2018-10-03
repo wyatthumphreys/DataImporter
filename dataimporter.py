f29de52a669fc2aa5eff90e022d42f898380f5c6
wyatthumphreys <38059353+wyatthumphreys@users.noreply.github.com>
2018-10-02 17:40:44 -0700

    Update dataimporter.py

--- dataimporter.py
+++ dataimporter.py
@@ -11,11 +11,11 @@ con = sqlite3.connect("Public_Computer_Access_Locations.db")
 cur = con.cursor()
 cur.execute("CREATE TABLE t (Lab_name, Phone, Accessible, Hours, Tech_Support_Assisted, Organization, Location, Web_address);") # use your column names here
 
-with open('Public_Computer_Access_Locations.csv','r') as fin: # `with` statement available in 2.5+
+with open('Public_Computer_Access_Locations.csv','r') as fin: # Open the spreadsheet and give it the variable name "fin"
     #csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['Lab_name'], i['Phone'], i['Accessible'], i['Hours'], i['Tech_Support_Assisted'], i['Organization'], i['Location'], i['Web_address']) for i in dr]
+    dr = csv.DictReader(fin) # reads a row from the csv and maps the info to an OrderedDict container. Also preserves order.
+    to_db = [(i['Lab_name'], i['Phone'], i['Accessible'], i['Hours'], i['Tech_Support_Assisted'], i['Organization'], i['Location'], i['Web_address']) for i in dr] #Defines to_db as for each entry (i) in dr, make a list with entries in correct order.
 
-cur.executemany("INSERT INTO t (Lab_name, Phone, Accessible, Hours, Tech_Support_Assisted, Organization, Location, Web_address) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", to_db) # ? are used for number of items
+cur.executemany("INSERT INTO t (Lab_name, Phone, Accessible, Hours, Tech_Support_Assisted, Organization, Location, Web_address) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", to_db) # Plug in values from the to_db list into the columns of the 't' table based on their order from line 17. The '?' are used for number of items
 con.commit()
 con.close()
